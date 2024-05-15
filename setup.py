30d7111b612d6540d8d80e0296b87eb3b4905104
hrshankar2002 <hrshankar2002@gmail.com>
2024-05-15 21:25:57 +0530

    updated dependencies

--- setup.py
+++ setup.py
@@ -2,17 +2,6 @@ from typing import List
 
 from setuptools import find_packages, setup
 
-HYPEN_E_DOT='-e .'
-
-def get_requirements(file_path:str) -> List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
-
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
@@ -36,7 +25,7 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirements('./requirements_dev.txt'),
+    install_requires=['psycopg2-binary', 'mysql-connector-python', 'pymongo', 'pymongo[srv]', 'dnspython', 'ensure', 'pytest==7.1.3', 'tox', 'black==22.8.0', 'flake8==5.0.4', 'mypy==0.971']
     )
 
 
